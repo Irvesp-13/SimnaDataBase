8282ed9598fa192ccc5b56db14c02cda8e549f91
Irving Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2024-03-28 22:29:43 -0600

    Arregle el SimnaDatabaseApplication

--- src/main/java/mx/edu/utez/simnaDatabase/SimnaDatabaseApplication.java
+++ src/main/java/mx/edu/utez/simnaDatabase/SimnaDatabaseApplication.java
@@ -22,4 +22,16 @@ public class SimnaDatabaseApplication {
 		};
 	}
 
+	/*
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+	 */
+
 }
